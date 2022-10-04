8511ff4c8438bf38cb93ae66e7f150ffe2beb939
Eric Nedervold <nedervoldsoftware@gmail.com>
2022-10-04 14:29:50 -0700

    Added a JSON roundtripping test for Config.

--- src/Config.hs
+++ src/Config.hs
@@ -2,16 +2,25 @@
 
 module Config
   ( Config(..)
+  , genConfig
   , readConfig
   ) where
 
 import Data.Aeson.TH
+import Hedgehog (Gen)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import Names (upperCamelToLowerCamel)
 
 data Config = Config
   { configLanguagePrefix :: Maybe String
   , configBuildFilePath :: Maybe FilePath
-  } deriving (Show)
+  } deriving (Eq, Show)
+
+genConfig :: Gen Config
+genConfig =
+  Config <$> Gen.maybe (Gen.string (Range.linear 1 10) Gen.ascii) <*>
+  Gen.maybe (Gen.string (Range.linear 1 10) Gen.ascii)
 
 deriveJSON
   defaultOptions
---
+++ test/ConfigSpec.hs
@@ -0,0 +1,12 @@
+module ConfigSpec where
+
+import Config (genConfig)
+import Hedgehog.Classes (jsonLaws, lawsCheck)
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec_typeclasses :: Spec
+spec_typeclasses = do
+  describe "Config" $
+    it "roundtrips properly to JSON" $ do
+      passed <- lawsCheck $ jsonLaws genConfig
+      passed `shouldBe` True
